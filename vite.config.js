48c73300d70f123f36a8055aa8aad6c66df19d01
shonibare omofolarin <folarinshonibare@yahoo.com>
2024-07-17 02:48:09 +0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: parseInt(process.env.PORT) || 8080,
+  },
   define: {
     "process.env": process.env,
   },
